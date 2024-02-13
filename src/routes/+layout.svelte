c94ea3a9547852355dfb27c0bc50bbc41f3e4e7e
abdelhalim <rioman19@gmail.com>
2024-02-12 23:12:32 -0500

    make page scrollable

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,14 @@
 	import './app.css';
 </script>
 
-<div class="app-root">
+<div class="app-root scrollbar-none">
 	<slot />
 </div>
 
 <style>
 	.app-root {
-		@apply w-[100svw] h-[100svh] overflow-hidden bg-neutral-200 tracking-[-0.07rem] pt-14;
+		@apply w-[100svw] h-[100svh] overflow-hidden overflow-y-scroll bg-neutral-200 tracking-[-0.07rem] pt-14;
+
+		max-height: 100svh;
 	}
 </style>
